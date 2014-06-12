4e24947ffb0e1644f631899fb9cb127aaebb127e
Marko Grešak <gresak.marko@gmail.com>
2014-06-13 01:44:00 +0200

    Dodano ogrodje in test za 4. komplet, 1. nalogo

---
+++ ogrodja in testi/izpiti-4-1.c
@@ -0,0 +1,18 @@
+// izpiti komplet 4/5 - naloga 1.
+
+// zazeni z ukazom v terminalu:
+// gcc -std=c99 izpiti-4-1.c -o izpiti-4-1 && ./izpiti-4-1 < input-1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+// naloga zahteva celoten program, zato tukaj dodatne kode ni,
+// podan je samo testni primer iz besedila (navodila za test so zgoraj)
+int main(int argc, char const *argv[])
+{
+  
+  return 0;
+}
